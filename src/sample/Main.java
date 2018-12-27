0ffa6e808ca798d9b4d13eeabf28dc37f42f2708
Justin Golden <justingolden21@gmail.com>
2018-12-27 00:27:58 -0800

    add icons (yay)

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
@@ -11,13 +12,18 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     Button d4button;
-
-
+    
     Label rollLabel;
 
     @Override
     public void start(Stage window) throws Exception {
         window.setTitle("Die Roller");
+
+        window.getIcons().addAll(
+                new Image("/img/icon64.png"),
+                new Image("/img/icon32.png"),
+                new Image("/img/icon16.png")
+        );
 
         d4button = new Button();
         d4button.setText("D4");
