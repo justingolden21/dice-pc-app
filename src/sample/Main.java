23d9efc35e2008999a3f33936482bfa02b4464ed
Justin Golden <justingolden21@gmail.com>
2018-12-26 23:36:38 -0800

    cleaned the code a bit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,22 +1,17 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     Button button;
 
-//    Text text;
 
     Label rollLabel;
 
@@ -25,25 +20,18 @@ public class Main extends Application {
         window.setTitle("Die Roller");
 
         button = new Button();
-        button.setText("Click me");
+        button.setText("D4");
         button.setOnAction(e -> {
-            rollLabel.setText( Integer.toString(Util.getRoll(4) ) );
+            rollLabel.setText( "Rolled D4: " + Integer.toString(Util.getRoll(4) ) );
         });
 
-//        text = new Text();
         rollLabel = new Label("Roll: ");
-//        text.setText(rollLabel);
-
-
-
 
         BorderPane layout = new BorderPane();
         HBox hbox = new HBox();
         layout.setTop(hbox);
         layout.setBottom(button);
         layout.setCenter(rollLabel);
-
-//        layout.getChildren().addAll(button, rollLabel);
 
         Scene scene = new Scene(layout, 300, 250);
         window.setScene(scene);
