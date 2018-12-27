bce1c3118b6a43f52c156a2529f311449e539a7d
Justin Golden <justingolden21@gmail.com>
2018-12-27 00:38:18 -0800

    add multiple roll buttons

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,19 +6,21 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    Button d4button;
-    
-    Label rollLabel;
+    Button d4button, d6button, d8button;
+
+    public static Label rollLabel;
 
     @Override
     public void start(Stage window) throws Exception {
         window.setTitle("Die Roller");
 
+        //http://iconhandbook.co.uk/reference/chart/windows/
         window.getIcons().addAll(
                 new Image("/img/icon64.png"),
                 new Image("/img/icon32.png"),
@@ -26,17 +28,31 @@ public class Main extends Application {
         );
 
         d4button = new Button();
+        d6button = new Button();
+        d8button = new Button();
+
         d4button.setText("D4");
+        d6button.setText("D6");
+        d8button.setText("D8");
+
         d4button.setOnAction(e -> {
-            rollLabel.setText( "Rolled D4: " + Integer.toString(Util.getRoll(4) ) );
+            Util.displayRoll(4);
+        });
+        d6button.setOnAction(e -> {
+            Util.displayRoll(6);
+        });
+        d8button.setOnAction(e -> {
+            Util.displayRoll(8);
         });
 
         rollLabel = new Label("Roll: ");
 
         BorderPane layout = new BorderPane();
-        HBox hbox = new HBox();
-        layout.setTop(hbox);
-        layout.setBottom(d4button);
+
+        FlowPane flowpane = new FlowPane();
+        flowpane.getChildren().addAll(d4button, d6button, d8button);
+        
+        layout.setTop(flowpane);
         layout.setCenter(rollLabel);
 
         Scene scene = new Scene(layout, 300, 250);
--- src/sample/Util.java
+++ src/sample/Util.java
@@ -2,8 +2,12 @@ package sample;
 
 public class Util {
 
-    public static int getRoll(int sides) {
+    static int getRoll(int sides) {
         return (int) Math.floor(Math.random() * sides) + 1;
+    }
+
+    public static void displayRoll(int roll) {
+        Main.rollLabel.setText("Rolled D" + Integer.toString(roll) + ": " + Integer.toString(getRoll(roll) ) );
     }
 
 }
