641f499974c281bb8e789784eb2f33a2f96d9334
Justin Golden <justingolden21@gmail.com>
2018-12-27 01:14:40 -0800

    add roll many times

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,9 +1,12 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
@@ -11,9 +14,15 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    final int WINDOW_WIDTH = 250;
+    final int WINDOW_HEIGHT = 250;
+
+
     Button d4button, d6button, d8button;
 
     public static Label rollLabel;
+
+    public static TextField numDice;
 
     @Override
     public void start(Stage window) throws Exception {
@@ -35,26 +44,42 @@ public class Main extends Application {
         d8button.setText("D8");
 
         d4button.setOnAction(e -> {
-            Util.displayRoll(4);
+            Util.doRolls(4);
         });
         d6button.setOnAction(e -> {
-            Util.displayRoll(6);
+            Util.doRolls(6);
         });
         d8button.setOnAction(e -> {
-            Util.displayRoll(8);
+            Util.doRolls(8);
         });
 
         rollLabel = new Label("Roll: ");
 
+        numDice = new TextField();
+        numDice.setText("1");
+
         BorderPane layout = new BorderPane();
 
         FlowPane flowpane = new FlowPane();
-        flowpane.getChildren().addAll(d4button, d6button, d8button);
+        flowpane.getChildren().addAll(numDice, d4button, d6button, d8button);
 
         layout.setTop(flowpane);
-        layout.setCenter(rollLabel);
+//        layout.setCenter(numDice);
+        layout.setBottom(rollLabel);
 
-        Scene scene = new Scene(layout, 300, 250);
+        // force the field to be numeric only
+        //https://stackoverflow.com/questions/7555564/what-is-the-recommended-way-to-make-a-numeric-textfield-in-javafx
+        numDice.textProperty().addListener( e ->  {
+            String textVal = numDice.getText();
+            if(!textVal.matches("\\d*")) {
+                numDice.setText(textVal.replaceAll("[^\\d]", ""));
+            }
+            if(textVal.length()>4) { //4 is max digits for 1000 which is maxRolls
+                numDice.setText(textVal.substring(0,4) );
+            }
+        });
+
+        Scene scene = new Scene(layout, WINDOW_WIDTH, WINDOW_HEIGHT);
         window.setScene(scene);
         window.show();
     }
--- src/sample/Util.java
+++ src/sample/Util.java
@@ -1,13 +1,38 @@
 package sample;
 
+import java.lang.reflect.Array;
+
 public class Util {
+
+    static int maxRolls = 1000;
 
     static int getRoll(int sides) {
         return (int) Math.floor(Math.random() * sides) + 1;
     }
 
-    public static void displayRoll(int roll) {
-        Main.rollLabel.setText("Rolled D" + Integer.toString(roll) + ": " + Integer.toString(getRoll(roll) ) );
+    public static void doRolls(int sides) {
+
+        int numRolls;
+        try {
+            numRolls = Integer.parseInt(Main.numDice.getText() );
+        } catch(NumberFormatException e) {
+            numRolls = 1;
+        }
+        numRolls = Math.max(Math.min(maxRolls, numRolls), 1);
+        Main.numDice.setText(Integer.toString(numRolls) );
+
+        int[] rolls = new int[numRolls];
+        int sum = 0;
+        String rollList = "(";
+        for(int i=0; i<numRolls; i++) {
+            rolls[i] = getRoll(sides);
+            sum += rolls[i];
+            rollList += (i==0 ? "" : ", ") + Integer.toString(rolls[i]);
+        }
+        rollList += ")";
+
+        Main.rollLabel.setText("Rolled " + Integer.toString(numRolls) + "D" + Integer.toString(sides) + ": " + Integer.toString(sum) + " " + rollList );
+
     }
 
 }
