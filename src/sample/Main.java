2d66454c5c446ad0b222b87a38b37be8a1d368fa
Justin Golden <justingolden21@gmail.com>
2018-12-26 23:05:12 -0800

    set up project, testing
    testing: add button listener to print in console

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,15 +4,27 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    Button button;
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Die Roller");
+
+        button = new Button();
+        button.setText("Click me");
+        button.setOnAction(e -> System.out.println("hi") );
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
@@ -21,3 +33,4 @@ public class Main extends Application {
         launch(args);
     }
 }
+
